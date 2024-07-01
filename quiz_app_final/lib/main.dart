0b89dda173543f36425472d44083c47b05346bad
Ananyaejain <jainananyae@gmail.com>
2024-07-01 23:12:20 +0530

    Task2_QuizApp

---
+++ quiz_app_final/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'category_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyAppColors {
+  static final primaryColor = Colors.white;
+  static final secondaryColor = Colors.purple;
+}
+
+class MyAppThemes {
+  static final darkTheme = ThemeData(
+      primaryColor: MyAppColors.primaryColor,
+      scaffoldBackgroundColor: MyAppColors.primaryColor,
+      brightness: Brightness.light
+  );
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: MyAppThemes.darkTheme,
+      home: CategoryScreen(),
+    );
+  }
+}
